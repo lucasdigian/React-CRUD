c1fa12a8a7602640cac5cdd73848ba0bfe357d1a
Lucas <digian.santos@gmail.com>
2023-03-28 17:12:57 -0300

    change the function delete button the cards

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,30 +1,38 @@
-import React from "react"
-import "../style/maincontainer.sass"
-import PerfilCard from "./PerfilCard"
-import { useState} from "react";
-
+import React from "react";
+import "../style/maincontainer.sass";
+import PerfilCard from "./PerfilCard";
+import { useState } from "react";
 
 //Gerenciamento de Perfis
 const MainContainer = () => {
-  const [components,setComponents] = useState([])
+  const [components, setComponents] = useState([]);
 
-  function createElemente(){
-    if(components.length<4){
-      setComponents([...components,'Luis'])
-    }else{
-      alert('Quantidade maxima de perfis criado')
+  function createElemente() {
+    if (components.length < 4) {
+      setComponents([
+        ...components,
+        { id: components.length + 1, name: "luis" },
+      ]);
+    } else {
+      alert("Quantidade maxima de perfis criado");
     }
   }
 
-  return <div id="container">
-    <div className="button-wrapper align-end">
-          <button onClick={createElemente}>Criar Perfil</button>
-        </div>
+  const onDelete = id => {
+    const filteredUsers = components.filter(u => u.id !== id);
+    setComponents(filteredUsers);
+  }
 
-        {components.map((item) => (<PerfilCard name={item}/> ))}
-
+  return (
+    <div id="container">
+      <div className="button-wrapper align-end">
+        <button onClick={createElemente}>Criar Perfil</button>
+      </div>
+      {components.map((item) => (
+        <PerfilCard key={item.id} id={item.id} name={item.name} onDelete={onDelete}/>
+      ))}
     </div>
-}
+  );
+};
 
-export default MainContainer
-
+export default MainContainer;
--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,26 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+function changeValues(trigger){
+	trigger.change(function(){
+    switch($(trigger).val()){
+    case '1':
+        changeList(step1)
+        break
+    case '2':
+        changeList(step2)
+        break
+    case '3':
+        changeList(step3)
+        break
+    case '4':
+        changeList(step4)
+        break
+    case '5':
+        changeList(step5)
+        $('.hide-option5').hide()
+        break
+}
+})
+}
